c809a59aa0daec962fd2416a4107f71855532ebb
Mmerichukwu Anosike <67147759+Roarnotes@users.noreply.github.com>
2022-12-29 20:40:46 +0100

    Module 3 done

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,9 +1,9 @@
 import "./ExpenseDate.css";
 
-function ExpenseDate(dataProps) {
-    const month = dataProps.date.toLocaleString("en-US", { month: "long" });
-    const year = dataProps.date.getFullYear();
-    const day = dataProps.date.toLocaleString("en-US", { day: "2-digit" });
+function ExpenseDate(props) {
+    const month = props.date.toLocaleString("en-US", { month: "long" });
+    const year = props.date.getFullYear();
+    const day = props.date.toLocaleString("en-US", { day: "2-digit" });
 
     return (
       <div className="expense-date">
